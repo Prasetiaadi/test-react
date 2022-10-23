e0984831f83175b292ffbd350df6e995713b3cd6
Prasetiaadi <pras.adi.prakasa@gmail.com>
2022-10-23 20:50:25 +0700

    test

---
+++ src/components/intro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Intro(props) {
+  return (
+    <div>
+      <h1>Hello world..!!</h1>
+      <p>Hallo nama saya: {props.name}</p>
+    </div>
+  );
+}
+
+export default Intro;
